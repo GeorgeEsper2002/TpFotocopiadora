e217fe3a9669e4f4cf03d9562c8290d6dca9c97c
unknown <rachidesper290@gmail.com>
2024-10-07 19:20:58 -0300

    Se genero la clase PrintJob y se agregaron sus atributos

--- src/models/PrintJob.java
+++ src/models/PrintJob.java
@@ -2,4 +2,40 @@ package models;
 
 public class PrintJob {
 
+	private String description;
+	private int copies;
+	private String state;
+	private String quality;
+	
+	
+	
+	
+	
+	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getCopies() {
+		return copies;
+	}
+	public void setCopies(int copies) {
+		this.copies = copies;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getQuality() {
+		return quality;
+	}
+	public void setQuality(String quality) {
+		this.quality = quality;
+	}
+
+	
 }
