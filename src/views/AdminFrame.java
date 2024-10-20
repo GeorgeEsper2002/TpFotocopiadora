db37fb6fb6d0d9a1afc7043542cdfeed8793b59f
unknown <rachidesper290@gmail.com>
2024-10-20 18:20:10 -0300

    se organizo el tp

--- src/views/AdminFrame.java
+++ src/views/AdminFrame.java
@@ -1,5 +1,22 @@
 package views;
 
+
+/*
+ *	Programar botones de:
+ *	
+ *	// Referido a usuarios
+ *	- Agregar Usuarios
+ *	- Eliminar Usuarios
+ *	- Desactivar Usuarios
+ *
+ *	// Referido a los trabajos
+ *	- Listar todos los trabajos que hay
+ *	- Manipular los trabajos,con su estado
+ *	- Ver trabajos que hay POR USUARIO
+ * 
+ * */
+
+
 public class AdminFrame {
 
 }
