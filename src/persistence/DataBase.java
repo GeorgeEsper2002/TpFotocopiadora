b99345c68b4a15156a49481fd8166dcfada1f963
unknown <rachidesper290@gmail.com>
2024-10-23 17:21:57 -0300

    Se creo la base de datos

--- src/persistence/DataBase.java
+++ src/persistence/DataBase.java
@@ -1,7 +1,33 @@
 package persistence;
 
-public class DataBase {
+import models.User;
+import models.PrintJob;
 
-	// Hacer singleton con el arreglo
+public class DataBase{
 	
+	private static DataBase instance;
+	private User[] userDB;
+	private PrintJob[] printJobDB;
+	
+	private DataBase(){
+		
+		userDB=new User[10];
+		printJobDB=new PrintJob[20];
+		
+		userDB[0]=new User("admin","admin",0);
+		userDB[1]=new User("user1","user1",1);
+		userDB[2]=new User("user2","user2",1);
+		
+		printJobDB[0]=new PrintJob(1,"descripcion",22,"estado","byn","10/10/24","12/10/24","15/10/24");
+	}
+	
+	// SINGLETON
+	public static DataBase getInstance() {
+		if(instance==null) {
+			instance=new DataBase();
+			
+			
+		}
+		return instance;
+	} 
 }
