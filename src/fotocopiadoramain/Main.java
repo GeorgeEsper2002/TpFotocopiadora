f64010f9ef10572f12aadd9ff5bdcd83c708d782
unknown <rachidesper290@gmail.com>
2024-10-23 17:23:57 -0300

    ss

--- src/fotocopiadoramain/Main.java
+++ src/fotocopiadoramain/Main.java
@@ -1,13 +1,14 @@
 package fotocopiadoramain;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-import views.LoginFrame;
+import views.LoginView;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		LoginFrame ventanaLogin= new LoginFrame();
+		//LoginFrame ventanaLogin= new LoginFrame();
 		//JOptionPane.showMessageDialog(null,"Hola mundo");
+		LoginView login= new LoginView();
 	}
 
 }
