9d60972b74c4f5d7a2c739770f94c6d90fdc287a
unknown <rachidesper290@gmail.com>
2024-10-30 17:39:48 -0300

    prueba de ventana de admin

--- src/fotocopiadoramain/Main.java
+++ src/fotocopiadoramain/Main.java
@@ -1,16 +1,18 @@
 package fotocopiadoramain;
+
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import views.LoginView;
+import views.AdminView;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//LoginFrame ventanaLogin= new LoginFrame();
-		//JOptionPane.showMessageDialog(null,"Hola mundo");
-		LoginView login= new LoginView();
+		// LoginFrame ventanaLogin= new LoginFrame();
+		// JOptionPane.showMessageDialog(null,"Hola mundo");
+		// LoginView login= new LoginView();
+		AdminView admin = new AdminView();
 	}
 
 }
-
-
