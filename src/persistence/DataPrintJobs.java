b219524685b68971df456b6b3a0df5faf19fc380
unknown <rachidesper290@gmail.com>
2024-10-31 10:01:34 -0300

    modified printJob database

--- src/persistence/DataPrintJobs.java
+++ src/persistence/DataPrintJobs.java
@@ -10,7 +10,7 @@ public class DataPrintJobs {
 
 	private DataPrintJobs() {
 		printJobDB = new PrintJob[20];
-		printJobDB[0] = new PrintJob(1, "descripcion", 22, "estado", "byn", "10/10/24", "12/10/24", "15/10/24", null);
+		printJobDB[0] = new PrintJob(1, "descripcion", 22, "estado", "byn", "10/10/24", "12/10/24", "15/10/24", null,null);
 
 	}
 
