4388cdda9d27082cc371bcc2f8b1546e623faace
unknown <rachidesper290@gmail.com>
2024-11-08 02:18:32 -0300

    Ventana crear trabajos funcionando

--- src/views/printjob/PrintJobView.java
+++ src/views/printjob/PrintJobView.java
@@ -14,130 +14,130 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import models.User;
+import persistence.DataUser;
+import service.PrintJobService;
 import views.user.UserView;
 
 // Vista para agregar Trabajos
 public class PrintJobView implements ActionListener {
 
-	private JButton create,clean,back;
+	private JButton create, clean, back;
 	private JFrame frame;
-	private JComboBox comboQuality;
-	private JTextField txtId,txtDescription,txtCopies;
-	private JLabel lblDesc,lblCopies,lblQuality; 
-	public PrintJobView() {
-		
-		frame=new JFrame("Ventana de Usuario-User");
+	private JComboBox<String> comboQuality;
+	private JTextField txtDescription, txtCopies;
+	private JLabel lblDesc, lblCopies, lblQuality;
+	private User currentUser;
+
+	public PrintJobView(String userName) {
+		currentUser = DataUser.getLoggedUser(userName);
+		frame = new JFrame("Ventana de Usuario-" + currentUser.getName());
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		JPanel titleBar = new JPanel(new BorderLayout());
-        JLabel titleLabel = new JLabel("Agregar Trabajo", JLabel.CENTER);
-        titleBar.add(titleLabel, BorderLayout.CENTER);
+		JLabel titleLabel = new JLabel("Agregar Trabajo", JLabel.CENTER);
+		titleBar.add(titleLabel, BorderLayout.CENTER);
 
 		Toolkit screen = Toolkit.getDefaultToolkit();
 		Dimension dimension = screen.getScreenSize();
 
-		frame.setSize(800,600);
+		frame.setSize(800, 600);
 		frame.setLocationRelativeTo(null);
 
 		Image icon = screen.getImage("src/views/java.png");
 		frame.setIconImage(icon);
 		frame.add(titleBar, BorderLayout.NORTH);
-		
-		 // Base panel
-        JPanel basePanel = new JPanel(new BorderLayout());
 
-        // Specific panel for adding printJobs
-        JPanel addPrintJobPanel = new JPanel(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(10, 10, 10, 10);
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-		
-		
-        lblDesc= new JLabel("Titulo del trabajo: ");
-        addPrintJobPanel.add(lblDesc, gbc);
+		// Base panel
+		JPanel basePanel = new JPanel(new BorderLayout());
 
-        gbc.gridx = 1;
-        txtDescription = new JTextField(20);
-        addPrintJobPanel.add(txtDescription, gbc);
+		// Specific panel for adding printJobs
+		JPanel addPrintJobPanel = new JPanel(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.insets = new Insets(10, 10, 10, 10);
+		gbc.gridx = 0;
+		gbc.gridy = 0;
 
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        lblCopies = new JLabel("Cantidad de copias:");
-        addPrintJobPanel.add(lblCopies, gbc);
+		lblDesc = new JLabel("Titulo del trabajo: ");
+		addPrintJobPanel.add(lblDesc, gbc);
 
-        gbc.gridx = 1;
-        txtCopies = new JTextField(20);
-        addPrintJobPanel.add(txtCopies, gbc);
-        
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        lblQuality = new JLabel("Calidad:");
-        addPrintJobPanel.add(lblQuality, gbc);
+		gbc.gridx = 1;
+		txtDescription = new JTextField(20);
+		addPrintJobPanel.add(txtDescription, gbc);
 
-        gbc.gridx = 1;
-        String[] quality = {"Color", "Blanco y Negro"};
-        comboQuality = new JComboBox<>(quality);
-        addPrintJobPanel.add(comboQuality, gbc);
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		lblCopies = new JLabel("Cantidad de copias:");
+		addPrintJobPanel.add(lblCopies, gbc);
 
-        // Button to create Jobs
-        gbc.gridy = 3;
-        gbc.gridx=0;
-        create = new JButton("Crear");
-        create.setFocusable(false);
-        create.addActionListener(this);
-        addPrintJobPanel.add(create, gbc);
+		gbc.gridx = 1;
+		txtCopies = new JTextField(20);
+		addPrintJobPanel.add(txtCopies, gbc);
 
-        // Button to clear textfields
-        gbc.gridy = 3;
-        gbc.gridx=1;
-        clean = new JButton("Limpiar");
-        clean.setFocusable(false);
-        clean.addActionListener(this);
-        addPrintJobPanel.add(clean, gbc);
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		lblQuality = new JLabel("Calidad:");
+		addPrintJobPanel.add(lblQuality, gbc);
 
-        // Button to go back
-        gbc.gridy = 3;
-        gbc.gridx=2;
-        back = new JButton("Atras");
-        back.setFocusable(false);
-        back.addActionListener(this);
-        addPrintJobPanel.add(back, gbc);
-        
-        
-		
-		
-		
-		
-		
-		
+		gbc.gridx = 1;
+		String[] quality = { "Color", "Blanco y Negro" };
+		comboQuality = new JComboBox<>(quality);
+		addPrintJobPanel.add(comboQuality, gbc);
+
+		// Button to create Jobs
+		gbc.gridy = 3;
+		gbc.gridx = 0;
+		create = new JButton("Crear");
+		create.setFocusable(false);
+		create.addActionListener(this);
+		addPrintJobPanel.add(create, gbc);
+
+		// Button to clear textfields
+		gbc.gridy = 3;
+		gbc.gridx = 1;
+		clean = new JButton("Limpiar");
+		clean.setFocusable(false);
+		clean.addActionListener(this);
+		addPrintJobPanel.add(clean, gbc);
+
+		// Button to go back
+		gbc.gridy = 3;
+		gbc.gridx = 2;
+		back = new JButton("Atras");
+		back.setFocusable(false);
+		back.addActionListener(this);
+		addPrintJobPanel.add(back, gbc);
+
 		frame.add(addPrintJobPanel);
 		frame.setVisible(true);
-		
-		
+
 	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if(e.getSource()==create) {
-			System.out.println("Crear Trabajos");
-			
-		}
-		else if(e.getSource()==clean){
-			
-			txtCopies.setText("");
-			txtDescription.setText("");
-			
-		}
-		else if(e.getSource()==back) {
+		if (e.getSource() == create) {
+
+			String answer = PrintJobService.createPrintJob(currentUser, txtDescription.getText(), txtCopies.getText(),
+					comboQuality.getSelectedItem().toString());
+			JOptionPane.showMessageDialog(null, answer);
+			clearText();
+		} else if (e.getSource() == clean) {
+			clearText();
+
+		} else if (e.getSource() == back) {
 			frame.dispose();
-			UserView user=new UserView();
+			UserView user = new UserView(currentUser.getName());
 		}
 	}
-	
-	
+
+	private void clearText() {
+		txtCopies.setText("");
+		txtDescription.setText("");
+	}
 }
