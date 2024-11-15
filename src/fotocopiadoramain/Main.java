a7f4a88115d15d04daff54e735e2d187dd984462
unknown <rachidesper290@gmail.com>
2024-11-15 02:27:38 -0300

    prueba

--- src/fotocopiadoramain/Main.java
+++ src/fotocopiadoramain/Main.java
@@ -6,6 +6,7 @@ import javax.swing.JOptionPane;
 import models.PrintJob;
 import views.LoginView;
 import views.admin.AdminView;
+import views.printjob.ListPrintJobs;
 import views.printjob.PrintJobView;
 import views.user.ListUsers;
 import views.user.UserView;
@@ -15,11 +16,12 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// JOptionPane.showMessageDialog(null,"Hola mundo");
-		 LoginView login= new LoginView();
+		LoginView login= new LoginView();
 		//AdminView admin = new AdminView();
 		//UserView user=new UserView();
 		//PrintJobView print=new PrintJobView();
 		//ListUsers list=new ListUsers();
+		//ListPrintJobs list=new ListPrintJobs("");
 	}
 
 }
