696819352865a0f5d230a972904cd9e988a8fe01
unknown <rachidesper290@gmail.com>
2024-11-15 02:26:48 -0300

    creada funcion de mostrar trabajos

--- src/service/PrintJobService.java
+++ src/service/PrintJobService.java
@@ -67,4 +67,39 @@ public class PrintJobService {
 		return idGreater + 1;
 	}
 
+	/*
+	 *  private int id;
+		private String description;
+		private String copies;
+		private String state;
+		private String quality;
+		private String entryDate;
+		private String finishDate;
+		private String deliveryDate;
+		private User user;
+		private User handler;//encargado
+
+	 */
+	
+	
+	public static String[][] listPrintJob(){
+		PrintJob[] printJobDB=DataPrintJobs.getInstance().getPrintJobDB();
+		String[][] dataPrintJob=new String[printJobDB.length][10];
+		for(int i = 0;i < printJobDB.length;i++) {
+			dataPrintJob[i][0]=String.valueOf(printJobDB[i].getId());
+			dataPrintJob[i][1]=String.valueOf(printJobDB[i].getCopies());
+			dataPrintJob[i][2]=printJobDB[i].getState();
+			dataPrintJob[i][3]=printJobDB[i].getQuality();
+			dataPrintJob[i][4]=printJobDB[i].getEntryDate();
+			dataPrintJob[i][5]=printJobDB[i].getFinishDate();
+			dataPrintJob[i][6]=printJobDB[i].getDeliveryDate();
+			dataPrintJob[i][7]=printJobDB[i].getUser().getName();
+			dataPrintJob[i][8]=printJobDB[i].getHandler().getName();
+			
+		}
+		return dataPrintJob;
+		
+		
+		
+	}
 }
