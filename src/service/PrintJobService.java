5c956ef949fac1c4aa38bb401b21313788ae6f23
unknown <rachidesper290@gmail.com>
2024-11-16 16:34:57 -0300

    Creada funcion para listar trabajos por user

--- src/service/PrintJobService.java
+++ src/service/PrintJobService.java
@@ -29,7 +29,7 @@ public class PrintJobService {
 			PrintJob job = new PrintJob();
 			job.setDescription(description);
 			job.setId(createId(job));
-			job.setState("hola");
+			job.setState("Recibido");
 			job.setHandler(DataUser.getAdmin());
 			job.setCopies(copies);
 			job.setUser(user);
@@ -67,19 +67,6 @@ public class PrintJobService {
 		return idGreater + 1;
 	}
 
-	/*
-	 *  private int id;
-		private String description;
-		private String copies;
-		private String state;
-		private String quality;
-		private String entryDate;
-		private String finishDate;
-		private String deliveryDate;
-		private User user;
-		private User handler;//encargado
-
-	 */
 	
 	
 	public static String[][] listPrintJob(){
@@ -87,14 +74,15 @@ public class PrintJobService {
 		String[][] dataPrintJob=new String[printJobDB.length][10];
 		for(int i = 0;i < printJobDB.length;i++) {
 			dataPrintJob[i][0]=String.valueOf(printJobDB[i].getId());
-			dataPrintJob[i][1]=String.valueOf(printJobDB[i].getCopies());
-			dataPrintJob[i][2]=printJobDB[i].getState();
-			dataPrintJob[i][3]=printJobDB[i].getQuality();
-			dataPrintJob[i][4]=printJobDB[i].getEntryDate();
-			dataPrintJob[i][5]=printJobDB[i].getFinishDate();
-			dataPrintJob[i][6]=printJobDB[i].getDeliveryDate();
-			dataPrintJob[i][7]=printJobDB[i].getUser().getName();
-			dataPrintJob[i][8]=printJobDB[i].getHandler().getName();
+			dataPrintJob[i][1]=printJobDB[i].getDescription();
+			dataPrintJob[i][2]=String.valueOf(printJobDB[i].getCopies());
+			dataPrintJob[i][3]=printJobDB[i].getState();
+			dataPrintJob[i][4]=printJobDB[i].getQuality();
+			dataPrintJob[i][5]=printJobDB[i].getEntryDate();
+			dataPrintJob[i][6]=printJobDB[i].getFinishDate();
+			dataPrintJob[i][7]=printJobDB[i].getDeliveryDate();
+			dataPrintJob[i][8]=printJobDB[i].getUser().getName();
+			dataPrintJob[i][9]=printJobDB[i].getHandler().getName();
 			
 		}
 		return dataPrintJob;
@@ -102,4 +90,36 @@ public class PrintJobService {
 		
 		
 	}
+	
+	public static String[][] listPrintJobsbyUser(String userName){
+		PrintJob[] printJobDB=DataBaseLogic.getPrintJobsByUser(userName);
+		String[][] dataPrintJob=new String[printJobDB.length][10];
+		for(int i = 0;i < printJobDB.length;i++) {
+			dataPrintJob[i][0]=String.valueOf(printJobDB[i].getId());
+			dataPrintJob[i][1]=printJobDB[i].getDescription();
+			dataPrintJob[i][2]=String.valueOf(printJobDB[i].getCopies());
+			dataPrintJob[i][3]=printJobDB[i].getState();
+			dataPrintJob[i][4]=printJobDB[i].getQuality();
+			dataPrintJob[i][5]=printJobDB[i].getEntryDate();
+			dataPrintJob[i][6]=printJobDB[i].getFinishDate();
+			dataPrintJob[i][7]=printJobDB[i].getDeliveryDate();
+			dataPrintJob[i][8]=printJobDB[i].getUser().getName();
+			dataPrintJob[i][9]=printJobDB[i].getHandler().getName();
+			
+		}
+		return dataPrintJob;
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
